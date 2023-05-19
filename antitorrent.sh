acb5f6a532363d999828e22f38e9d9422e5eac2f
akubudakgrik <133963148+akubudakgrik@users.noreply.github.com>
2023-05-19 16:58:23 +0800

    Add files via upload

--- antitorrent.sh
+++ antitorrent.sh
@@ -44,7 +44,6 @@ do
 	echo "You don't need to touch anything here"
 	echo "Just sit back and relax"
 	echo " "
-	echo " ### By akubudakgerik ###"
 	echo " "
 	echo "     [ MENU ]"
 	echo "1. Go Back"
