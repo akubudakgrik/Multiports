2e2a80243fac8f552a31f8e1056e70d7a48b3713
akubudakgrik <133963148+akubudakgrik@users.noreply.github.com>
2023-05-28 00:23:36 +0800

    Add files via upload

--- antitorrent.sh
+++ antitorrent.sh
@@ -44,7 +44,7 @@ do
 	echo "You don't need to touch anything here"
 	echo "Just sit back and relax"
 	echo " "
-	echo " ### By VPNBUDAKGERIK ###"
+	echo " ### By VPN-BUDAKGERIK ###"
 	echo " "
 	echo "     [ MENU ]"
 	echo "1. Go Back"
