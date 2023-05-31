495816d1df12787c403181a6d752ca311070477c
akubudakgrik <133963148+akubudakgrik@users.noreply.github.com>
2023-05-31 16:14:38 +0800

    Update websocket.sh

--- websocket-python/websocket.sh
+++ websocket-python/websocket.sh
@@ -10,41 +10,89 @@ GitUser="akubudakgrik"
 namafolder="websocket-python"
 #wget https://github.com/${GitUser}/main/${namafolder}/
 
-#System Websocket
+clear
+echo Installing Websocket-SSH Python
+sleep 1
+echo Sila Tunggu Sebentar...
+sleep 0.5
 cd
-cd /etc/systemd/system/
-wget -O /etc/systemd/system/cdn-ssl.service https://raw.githubusercontent.com/${GitUser}/Premium/main/${namafolder}/cdn-ssl.service
-#System Websocket-Ovpn Service
-cd /etc/systemd/system/
-wget -O /etc/systemd/system/cdn-ovpn.service https://raw.githubusercontent.com/${GitUser}/Premium/main/${namafolder}/cdn-ovpn.service
-#System Websocket-Dropbear Service
-cd /etc/systemd/system/
-wget -O /etc/systemd/system/cdn-dropbear.service https://raw.githubusercontent.com/${GitUser}/Premium/main/${namafolder}/cdn-dropbear.service
 
-#Install WS-SSL
-wget -q -O /usr/local/bin/cdn-ssl https://raw.githubusercontent.com/${GitUser}/Premium/main/${namafolder}/cdn-ssl.py
-chmod +x /usr/local/bin/cdn-ssl
-#Install WS-OpenVPN
-wget -q -O /usr/local/bin/cdn-ovpn https://raw.githubusercontent.com/${GitUser}/Premium/main/${namafolder}/cdn-ovpn.py
-chmod +x /usr/local/bin/cdn-ovpn
-#Install WS-Dropbear
-wget -q -O /usr/local/bin/cdn-dropbear https://raw.githubusercontent.com/${GitUser}/Premium/main/${namafolder}/cdn-dropbear.py
-chmod +x /usr/local/bin/cdn-dropbear
+# // GIT USER
+GitUser="CHEKS-HUNTER"
+namafolder="websocket-python"
 
-#Enable & Start & Restart ws-stunnel service
+# // SYSTEM WEBSOCKET HTTPS 443
+cat <<EOF> /etc/systemd/system/ws-https.service
+[Unit]
+Description=Python Proxy
+Documentation=https://github.com/CHEKS-HUNTER/
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+Restart=on-failure
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-https
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# // SYSTEM WEBSOCKET HTTP 80
+cat <<EOF> /etc/systemd/system/ws-http.service
+[Unit]
+Description=Python Proxy
+Documentation=https://github.com/CHEKS-HUNTER/
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVIC
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-http
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# // SYSTEM WEBSOCKET OVPN
+cat <<EOF> /etc/systemd/system/ws-ovpn.service
+[Unit]
+Description=Python Proxy
+Documentation=https://github.com/CHEKS-HUNTER/
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2097
+Restart=on-failure
+
+[Install]
+WantedBy=multi-use
+EOF
+
+# // PYTHON WEBSOCKET TLS && NONE
+wget -q -O /usr/local/bin/ws-https https://raw.githubusercontent.com/${GitUser}/Multiport/main/${namafolder}/ws-https; chmod +x /usr/local/bin/ws-https
+
+# // PYTHON WEBSOCKET DROPBEAR
+wget -q -O /usr/local/bin/ws-http https://raw.githubusercontent.com/${GitUser}/Multiport/main/${namafolder}/ws-http; chmod +x /usr/local/bin/ws-http
+
+# // PYTHON WEBSOCKET OVPN
+wget -q -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/Multiport/main/${namafolder}/ws-ovpn; chmod +x /usr/local/bin/ws-ovpn
+
+# // RESTART && ENABLE SSHVPN WEBSOCKET TLS 
 systemctl daemon-reload
-systemctl enable cdn-ssl
-systemctl start cdn-ssl
-systemctl restart cdn-ssl
-
-#Enable & Start & Restart ws-ovpn service
-systemctl daemon-reload
-systemctl enable cdn-ovpn
-systemctl start cdn-ovpn
-systemctl restart cdn-ovpn
-
-#Enable & Start & Restart ws-dropbear service
-systemctl daemon-reload
-systemctl enable cdn-dropbear
-systemctl start cdn-dropbear
-systemctl restart cdn-dropbear
+systemctl enable ws-https
+systemctl restart ws-https
+systemctl enable ws-http
+systemctl restart ws-http
+systemctl enable ws-ovpn
